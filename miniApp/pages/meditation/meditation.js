a9a0b74f3fecba04a948b0ed139a5f3d0b8746e6
ffzl <634555688@qq.com>
2020-02-14 12:13:17 +0800

    2.14

--- miniApp/pages/meditation/meditation.js
+++ miniApp/pages/meditation/meditation.js
@@ -1,13 +1,16 @@
 // pages/meditation/meditation.js
+//获取应用实例
+const imgs = require('../../utils/img')
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    columns: []
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +29,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('imgs....', imgs);
+    this.setData({
+      columns: [imgs.slice(0, 30), imgs.slice(30, 60), imgs.slice(60, 90)]
+    })
   },
 
   /**
@@ -47,20 +53,24 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('用户触发了下拉操作....');
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('用户触发了上拉加载操作.....');
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: '我的页面',
+      path: '/pages/meditation/meditation',
+      imageUrl: ''
+    }
   }
 })
